07fabe38eb08e506f1efd724db12a890c451e145
kbnd <hidrolyz@gmail.com>
2022-01-11 21:10:09 +0200

    last commit

--- js/app.js
+++ js/app.js
@@ -5,6 +5,33 @@
     t.addEventListener("click", () => {
       t.classList.toggle("_active");
     });
+  new Swiper(".benefits__slider", {
+    direction: "horizontal",
+    loop: !0,
+    pagination: { el: ".swiper-pagination" },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    speed: 800,
+    autoplay: { delay: 2e3 },
+  }),
+    new Swiper(".partners__slider", {
+      direction: "horizontal",
+      loop: !0,
+      pagination: { el: ".swiper-pagination" },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      speed: 800,
+      autoplay: { delay: 3200 },
+      slidesPerView: 3,
+      breakpoints: {
+        320: { slidesPerView: 2, spaceBetween: 20 },
+        580: { slidesPerView: 3, spaceBetween: 30 },
+      },
+    });
   let t = !1;
   setTimeout(() => {
     if (t) {
